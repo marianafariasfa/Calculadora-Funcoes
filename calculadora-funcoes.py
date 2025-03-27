1efbd85b290ea597e9cf8438b43fd4358bea7432
Mariana Farias <mrrfariass@gmail.com>
2025-03-27 08:35:53 -0300

    Add files via upload

---
+++ calculadora-funcoes.py
@@ -0,0 +1,68 @@
+import funcoes
+
+funcoes.limpar_tela()
+
+def obter_operador():
+    return float(input("\nEscolha: "))
+
+def operacao_soma(numero1,numero2):
+    return numero1 + numero2
+
+def operacao_subtracao(numero1,numero2):
+    return numero1 - numero2
+
+def operacao_divisao(numero1,numero2):
+    return numero1 / numero2
+
+def operacao_multiplicacao(numero1,numero2):
+    return numero1 * numero2
+
+status = True
+
+while status == True:
+    
+    print("-----CALCULADORA-----")
+
+    print("[1] - Soma")
+    print("[2] - Subtração")
+    print("[3] - Divisão")
+    print("[4] - Multiplicação")
+
+    operador = obter_operador()
+
+    funcoes.limpar_tela()
+   
+    numero1 = int(input("\nDigite o primeiro número para operação: "))
+    numero2 = int(input("\nDigite o segundo número para operação: "))
+
+    if operador == 1:
+        resultado1 = operacao_soma(numero1,numero2)
+        funcoes.limpar_tela()
+        print(f"O resultado é: ", resultado1)
+
+    elif operador == 2:
+        resultado2 = operacao_subtracao(numero1,numero2)
+        funcoes.limpar_tela()
+        print(f"O resultado é: ", resultado2)
+
+    elif operador == 3:
+        resultado3 = operacao_divisao(numero1,numero2)
+        funcoes.limpar_tela()
+        print(f"O resultado é: ", resultado3)
+
+    elif operador == 4:
+        resultado4 = operacao_multiplicacao(numero1,numero2)
+        funcoes.limpar_tela()
+        print(f"O resultado é: ", resultado4)
+
+    else:
+        funcoes.limpar_tela()
+        print("\nOpção inválida. Escolha entre 1 e 4.")
+
+    continuar = input("\nDeseja realizar outra operação? [S/N]: ", "\n").upper()  
+
+    if continuar == "N":
+        status = False  
+        print("\nFIM DO PROGRAMA")
+
+
